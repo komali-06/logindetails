42ab8e06d257308c2e94490976e9d7aa9480dfb5
komali-06 <64070332+komali-06@users.noreply.github.com>
2022-12-28 20:50:36 +0000

    Update HomeController.cs

--- LoginDetails/Controllers/HomeController.cs
+++ LoginDetails/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
-using System.Web.Script.Serialization;
+using System.Web.Serialization;
 using LoginDetails.LoginDetailsModels;
 using LoginDetails.OpenWeatherMapModels;
 using LoginDetails.Repositories;
